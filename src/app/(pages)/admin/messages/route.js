d500d82802a2cd1737c22669675507e5bb631abe
Saba Muhammad Riaz <sabaranicool94@gmail.com>
2025-07-26 23:49:06 +0500

    Update route.js

--- src/app/(pages)/admin/messages/route.js
+++ src/app/(pages)/admin/messages/route.js
@@ -1,16 +1,16 @@
-let messages = [];
+// let messages = [];
 
-export function GET() {
-  return new Response(JSON.stringify(messages), {
-    status: 200,
-  });
-}
+// export function GET() {
+//   return new Response(JSON.stringify(messages), {
+//     status: 200,
+//   });
+// }
 
-export async function POST(req) {
-  const body = await req.json();
-  messages.push({ ...body, date: new Date().toISOString() });
+// export async function POST(req) {
+//   const body = await req.json();
+//   messages.push({ ...body, date: new Date().toISOString() });
 
-  return new Response(JSON.stringify({ success: true }), {
-    status: 200,
-  });
-}
+//   return new Response(JSON.stringify({ success: true }), {
+//     status: 200,
+//   });
+// }
